4f5e709a4b44be9198df3dc483b2a7e8e8c327a3
Irishcalypso <karen@calypsoit.com>
2019-06-22 13:17:15 +0100

    first commit

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/api/posts',(req, res, next) => {
+  const posts =[
+    {
+      id: 'huhuy',
+      title:'first server-side post',
+      content:'this is coming for the server'
+    },
+    {
+      id: 'jgft',
+      title:'second server-side post',
+      content:'again this is coming for the server'
+    }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched successfully!',
+    posts: posts
+  });
+});
+
+module.exports = app;
